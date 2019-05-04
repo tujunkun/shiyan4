edab59c66a6915bba21fd1552cae3157c0bc6581
tjhk971128890117 <353607191@qq.com>
2019-05-04 23:40:04 +0800

    set

---
+++ src/main/java/com/example/shiyan4/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.shiyan4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+
